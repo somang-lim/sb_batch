a8d17509588aa71295d1211edb72ba1398bb5a5e
somang-lim <somangjuha@gmail.com>
2022-09-22 09:46:04 +0900

    배치앱으로 설정하여, 관련 스키마가 생기도록

--- src/main/java/com/ll/exam/sb_batch/SbBatchApplication.java
+++ src/main/java/com/ll/exam/sb_batch/SbBatchApplication.java
@@ -1,9 +1,11 @@
 package com.ll.exam.sb_batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SbBatchApplication {
 
 	public static void main(String[] args) {
