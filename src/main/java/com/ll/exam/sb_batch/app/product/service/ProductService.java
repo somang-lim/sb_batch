f45fc186e903ab173a9f10a992367fafa49d5c00
somang-lim <somangjuha@gmail.com>
2022-09-26 11:19:45 +0900

    샘플 상품 2개 생성

---
+++ src/main/java/com/ll/exam/sb_batch/app/product/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.ll.exam.sb_batch.app.product.service;
+
+import com.ll.exam.sb_batch.app.product.entity.Product;
+import com.ll.exam.sb_batch.app.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void create(String name, int price, String makerShopName) {
+        Product product = Product.builder()
+                .name(name)
+                .price(price)
+                .makerShopName(makerShopName)
+                .build();
+
+        productRepository.save(product);
+    }
+}
