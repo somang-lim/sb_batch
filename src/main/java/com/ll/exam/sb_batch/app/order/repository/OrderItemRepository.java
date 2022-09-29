e1a0af7d7982cbb151e9399e2d2760bf8f65337a
somang-lim <somangjuha@gmail.com>
2022-09-29 10:31:02 +0900

    범위 정산

--- src/main/java/com/ll/exam/sb_batch/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/exam/sb_batch/app/order/repository/OrderItemRepository.java
@@ -1,7 +1,10 @@
 package com.ll.exam.sb_batch.app.order.repository;
 
 import com.ll.exam.sb_batch.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    Page<OrderItem> findAllByLessThan(long id, Pageable pageable);
 }
